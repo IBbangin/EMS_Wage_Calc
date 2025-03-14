f5a06ce55a6c414d35491a0918a65a93f2443194
IBbangin <greil609@gmail.com>
2025-03-14 14:40:05 -0700

    Simplified rewrite

--- pay_calc.py
+++ pay_calc.py
@@ -1,323 +1,92 @@
-print("Welcome to the COCO EMS pay calculator!\n")
-wage = float(input("How much do you make per hour?\n"))
-days_wk1 = float(input("How many days did you work in the first week?\n"))
-overtimerate = float(wage*1.5)
-standardshift = float(12)
+class Pay_slip:
+    """a simple class to represent the pay for a week"""
+    def __init__(self,  regular_hours: float, overtime_hours: float, regular_pay: float, overtime_pay: float,):
+        # assign information provided to the new Payslip (an instance of the class)
+        self.regular_hours = regular_hours
+        self.overtime_hours = overtime_hours
+        self.regular_pay = regular_pay
+        self.overtime_pay = overtime_pay
 
-if days_wk1 >= 8:
-    print("Game Over")
-    exit()
-    
-
-# Daily Hour Totals
-if days_wk1 == 1:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(0)
-    day3 = float(0)
-    day4 = float(0)
-    day5 = float(0)
-    day6 = float(0)
-    day7 = float(0)
-
-if days_wk1 == 2:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(0)
-    day4 = float(0)
-    day5 = float(0)
-    day6 = float(0)
-    day7 = float(0)
-  
-
-if days_wk1 == 3:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(input("How many hours did you work on the third day?\n"))
-    day4 = float(0)
-    day5 = float(0)
-    day6 = float(0)
-    day7 = float(0)
-
-if days_wk1 == 4:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(input("How many hours did you work on the third day?\n"))
-    day4 = float(input("How many hours did you work on the fourth day?\n"))
-    day5 = float(0)
-    day6 = float(0)
-    day7 = float(0)
-
-if days_wk1 == 5:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(input("How many hours did you work on the third day?\n"))
-    day4 = float(input("How many hours did you work on the fourth day?\n"))
-    day5 = float(input("How many hours did you work on the fifth day?\n"))
-    day6 = float(0)
-    day7 = float(0)
-
-if days_wk1 == 6:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(input("How many hours did you work on the third day?\n"))
-    day4 = float(input("How many hours did you work on the fourth day?\n"))
-    day5 = float(input("How many hours did you work on the fifth day?\n"))
-    day6 = float(input("How many hours did you work on the sixth day?\n"))
-    day7 = float(0)
-
-if days_wk1 == 7:
-    day1 = float(input("How many hours did you work on the first day?\n"))
-    day2 = float(input("How many hours did you work on the second day?\n"))
-    day3 = float(input("How many hours did you work on the third day?\n"))
-    day4 = float(input("How many hours did you work on the fourth day?\n"))
-    day5 = float(input("How many hours did you work on the fifth day?\n"))
-    day6 = float(input("How many hours did you work on the sixth day?\n"))
-    day7 = float(input("How many hours did you work on the seventh day?\n"))
+    def __str__(self):  # how to print a Pay_slip
+        return ( # just returns a string so caller can decide to print or save to file
+            f"\tRegular hours: {self.regular_hours:.2f}\n"
+            f"\tOvertime hours: {self.overtime_hours:.2f}\n"
+            f"\tRegular pay: £{self.regular_pay:.2f}\n"
+            f"\tOvertime pay: £{self.overtime_pay:.2f}"
+        )
 
 
-days_wk2 = float(input("How many DAYS did you work in the second week?\n"))
-
-if days_wk2 >= 8:
-    print("Game Over")
-    exit()
-
-if days_wk2 == 1:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(0)
-    day10 = float(0)
-    day11 = float(0)
-    day12 = float(0)
-    day13 = float(0)
-    day14 = float(0)
-   
-
-if days_wk2 == 2:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(0)
-    day11 = float(0)
-    day12 = float(0)
-    day13 = float(0)
-    day14 = float(0)
-  
-
-if days_wk2 == 3:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(input("How many hours did you work on the third day?\n"))
-    day11 = float(0)
-    day12 = float(0)
-    day13 = float(0)
-    day14 = float(0)
-
-if days_wk2 == 4:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(input("How many hours did you work on the third day?\n"))
-    day11 = float(input("How many hours did you work on the fourth day?\n"))
-    day12 = float(0)
-    day13 = float(0)
-    day14 = float(0)
-
-if days_wk2 == 5:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(input("How many hours did you work on the third day?\n"))
-    day11 = float(input("How many hours did you work on the fourth day?\n"))
-    day12 = float(input("How many hours did you work on the fifth day?\n"))
-    day13 = float(0)
-    day14 = float(0)
-
-if days_wk2 == 6:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(input("How many hours did you work on the third day?\n"))
-    day11 = float(input("How many hours did you work on the fourth day?\n"))
-    day12 = float(input("How many hours did you work on the fifth day?\n"))
-    day13 = float(input("How many hours did you work on the sixth day?\n"))
-    day14 = float(0)
-
-if days_wk2 == 7:
-    day8 = float(input("How many hours did you work on the first day?\n"))
-    day9 = float(input("How many hours did you work on the second day?\n"))
-    day10 = float(input("How many hours did you work on the third day?\n"))
-    day11 = float(input("How many hours did you work on the fourth day?\n"))
-    day12 = float(input("How many hours did you work on the fifth day?\n"))
-    day13 = float(input("How many hours did you work on the sixth day?\n"))
-    day14 = float(input("How many hours did you work on the seventh day?\n"))
-
-mando_hr = float(input("How many hours of mandation did you work this pay period?\n"))
-
-# Daily overtime split for week one
+def extract_hours_worked(hours_entered: str) -> list[float]:
+    """returns a list of hours worked for each day worked of the week"""
+    try:  # in case some goes wrong converting
+        return [float(hours) for hours in hours_entered.split(",")]
+    except ValueError:  # something went wrong with converting a string to a float
+        raise ValueError("Expected hours worked with commas between hours for each day ")  # shout about it
 
 
+print("\n\nWelcome to the COCO EMS pay calculator!\n")
 
-if day1 == 0:
-    regularpay1 = float(0)
-    overtimehours1 = float(0)
-    overtimeincome1 = float(0)
-if 1 < day1 < 12:
-    regularpay1 = float(day1*wage)
-else: 
-    regularpay1 = float(12*wage)
-    overtimehours1 = float(day1 - standardshift)
-    overtimeincome1 = float(overtimehours1 * overtimerate)
+# need to establish hourly pay rate
+while True:  # input validation loop
+    try:
+        wage = float(input("How much do you make per hour? "))
+        if wage < 0 or wage > 100:
+            raise ValueError()
+        break  # leave input validation loop as have valid submission
+    except ValueError:
+        print("Expected an hourly wage between £0 and £100. Please try again.")
+print()
 
-if day2 == 0:
-    regularpay2 = float(0)   
-    overtimehours2 = float(0)
-    overtimeincome2 = float(0)
-if 1 < day2 < 12:
-    regularpay2 = float(day2*wage)
-else: 
-    regularpay2 = float(12*wage)
-    overtimehours2 = float(day2 - standardshift)
-    overtimeincome2 = float(overtimehours2 * overtimerate)
-    
-if day3 == 0:
-    regularpay3 = float(0)
-    overtimehours3 = float(0)
-    overtimeincome3 = float(0)
-if 1 < day3 < 12:
-    regularpay3 = float(day3*wage)
-else: 
-    regularpay3 = float(12*wage)
-    overtimehours3 = float(day3 - standardshift)
-    overtimeincome3 = float(overtimehours3 * overtimerate)
+OVERTIME_RATE = float(1.5)  # what to multiple wage by to get overtime pay
+STANDARD_SHIFT_HOURS = float(12)
 
-if day4 == 0:
-    regularpay4 = float(0)
-    overtimehours4 = float(0)
-    overtimeincome4 = float(0)
-if 1 < day4 < 12:
-    regularpay4 = float(day4*wage)
-else: 
-    regularpay4 = float(12*wage)
-    overtimehours4 = float(day4 - standardshift)
-    overtimeincome4 = float(overtimehours4 * overtimerate)
+NUM_WEEKS = 2  # how many weeks to calculate pay for
+weeks = []  # to store hours worked by day for each week
+weeks_pay = []  # to store payslips for each week
 
-if day5 == 0:
-    regularpay5 = float(0)
-    overtimehours5 = float(0)
-    overtimeincome5 = float(0)
-if 1 < day5 < 12:
-    regularpay5 = float(day5*wage)
-else: 
-    regularpay5 = float(12*wage)
-    overtimehours5 = float(day5 - standardshift)
-    overtimeincome5 = float(overtimehours5 * overtimerate)
+valid = True  # assume for now valid data will be provided
 
-if day6 == 0:
-    regularpay6 = float(0)
-    overtimehours6 = float(0)
-    overtimeincome6 = float(0)
-if 1 < day6 < 12:
-    regularpay6 = float(day6*wage)
-else: 
-    regularpay6 = float(12*wage)
-    overtimehours6 = float(day6 - standardshift)
-    overtimeincome6 = float(overtimehours6 * overtimerate)
+# loop for each week in period to be calculated
+for week_num in range(1, NUM_WEEKS + 1):
+    hours_entered = input(f"Enter hours worked in week {week_num} (comma separated, just return for none): ")
+    if hours_entered:  # an empty string is considered False byy Python
+        try:  # in case of invalid data
+            hours_worked = extract_hours_worked(hours_entered)
+            if len(hours_worked) > 7:
+                raise ValueError("Too many days of hours worked entered for a week" )
+        except ValueError as err_msg:
+            print(err_msg)
+            valid = False  # this will stop us producing payslips later
+            break  # leave loop, no point checking any more weeks
+    else:  # if no hours entered, perhaps on leave
+        hours_worked = [0]  # so set hours to 0, allows lists below to be correctly populated
+    weeks.append(hours_worked)
 
-if day7 == 0:
-    regularpay7 = float(0)
-    overtimehours7 = float(0)
-    overtimeincome7 = float(0)
-if 1 < day7 < 12:
-    regularpay7 = float(day7*wage)
-else: 
-    regularpay7 = float(12*wage)
-    overtimehours7 = float(day7 - standardshift)
-    overtimeincome7 = float(overtimehours7 * overtimerate)
+# if we had good data, can calculate hours, ot hours, and pay for each week
+if valid:  # only do this if we have valid data
+    for week in weeks:  # loop through data for each week
+        # calculate total regular and overtime hours for week
+        ot_hours = 0  # accumulate overtime hours for week
+        regular_hours = 0  # accumulate regular hours for week
+        for hours in week:  # loop through hours for current week
+            if hours > STANDARD_SHIFT_HOURS:
+                ot_hours += hours - STANDARD_SHIFT_HOURS  # accumulate overtime hours for week
+                regular_hours += STANDARD_SHIFT_HOURS  # accumulate regular hours for week
+            else:
+                regular_hours += hours  # accumulate regular hours for week
 
-# Daily Overtime Split for week 2
+        # calculate pay for week
+        regular_pay = regular_hours * wage
+        ot_pay = ot_hours * OVERTIME_RATE * wage
+        weeks_pay.append(Pay_slip(regular_hours, ot_hours, regular_pay, ot_pay))
 
+    # print payslips
+    for week_num, week in enumerate(weeks_pay, start=1):
+        print(
+            f"\nPayslip for week {week_num}:\n"
+            f"-------------------------------------\n"
+            f"{week}"
+        )
 
-if day8 == 0:
-    regularpay8 = float(0)
-    overtimehours8 = float(0)
-    overtimeincome8 = float(0)
-if 1 < day8 < 12:
-    regularpay8 = float(day8*wage)
-else: 
-    regularpay8 = float(12*wage)
-    overtimehours8 = float(day8 - standardshift)
-    overtimeincome8 = float(overtimehours8 * overtimerate)
-
-if day9 == 0:
-    regularpay9 = float(0)
-    overtimehours9 = float(0)
-    overtimeincome9 = float(0)
-if 1 < day9 < 12:
-    regularpay9 = float(day9*wage)
-else: 
-    regularpay9 = float(12*wage)
-    overtimehours9 = float(day2 - standardshift)
-    overtimeincome9 = float(overtimehours9 * overtimerate)
-
-if day10 == 0:
-    regularpay10 = float(0)
-    overtimehours10 = float(0)
-    overtimeincome10 = float(0)
-if 1 < day10 < 12:
-    regularpay10 = float(day10*wage)
-else: 
-    regularpay10 = float(12*wage)
-    overtimehours10 = float(day10 - standardshift)
-    overtimeincome10 = float(overtimehours10 * overtimerate)
-
-if day11 == 0:
-    regularpay11 = float(0)
-    overtimehours11 = float(0)
-    overtimeincome11 = float(0)
-if 1 < day11 < 12:
-    regularpay11 = float(day11*wage)
-else: 
-    regularpay11 = float(12*wage)
-    overtimehours11 = float(day11 - standardshift)
-    overtimeincome11 = float(overtimehours11 * overtimerate)
-
-if day12 == 0:
-    regularpay12 = float(0)
-    overtimehours12 = float(0)
-    overtimeincome12 = float(0)
-if 1 < day12 < 12:
-    regularpay12 = float(day12*wage)
-else: 
-    regularpay12 = float(12*wage)
-    overtimehours12 = float(day12 - standardshift)
-    overtimeincome12 = float(overtimehours12 * overtimerate)
-
-if day13 == 0:
-    regularpay13 = float(0)
-    overtimehours13 = float(0)
-    overtimeincome13 = float(0)
-if 1 < day13 < 12:
-    regularpay13 = float(day13*wage)
-else: 
-    regularpay13 = float(12*wage)
-    overtimehours13 = float(day13 - standardshift)
-    overtimeincome13 = float(overtimehours13 * overtimerate)
-
-if day14 == 0:
-    regularpay14 = float(0)
-    overtimehours14 = float(0)
-    overtimeincome14 = float(0)
-if 1 < day14 < 12:
-    regularpay14 = float(day14*wage)
-else: 
-    regularpay14 = float(12*wage)
-    overtimehours14 = float(day14 - standardshift)
-    overtimeincome14 = float(overtimehours14 * overtimerate)
-
-# Totals
-
-import locale
-locale.setlocale(locale.LC_ALL, 'C')
-def format_currency(regularpay1 , regularpay2 , regularpay3 , regularpay4 , regularpay5 , regularpay6 , regularpay7 , regularpay8 , regularpay9 , regularpay10 , regularpay11 , regularpay12 , regularpay13 , regularpay14 , overtimeincome1 , overtimeincome2 , overtimeincome3 , overtimeincome4 , overtimeincome5 , overtimeincome6 , overtimeincome7 , overtimeincome8 , overtimeincome9 , overtimeincome10 , overtimeincome11 , overtimeincome12 , overtimeincome13 , overtimeincome14):
-    return '${:,.2f}'.format(regularpay1 , regularpay2 , regularpay3 , regularpay4 , regularpay5 , regularpay6 , regularpay7 , regularpay8 , regularpay9 , regularpay10 , regularpay11 , regularpay12 , regularpay13 , regularpay14 , overtimeincome1 , overtimeincome2 , overtimeincome3 , overtimeincome4 , overtimeincome5 , overtimeincome6 , overtimeincome7 , overtimeincome8 , overtimeincome9 , overtimeincome10 , overtimeincome11 , overtimeincome12 , overtimeincome13 , overtimeincome14)
-
-#total_regular_pay = sum (4)
-#total_holdover_pay = sum (overtimeincome1 + overtimeincome2 + overtimeincome3 + overtimeincome4 + overtimeincome5 + overtimeincome6 + overtimeincome7 + overtimeincome8 + overtimeincome9 + overtimeincome10 + overtimeincome11 + overtimeincome12 + overtimeincome13 + overtimeincome14)
-
-total_regular_pay = regularpay1 + regularpay2 + regularpay3 + regularpay4 + regularpay5 + regularpay6 + regularpay7 + regularpay8 + regularpay9 + regularpay10 + regularpay11 + regularpay12 + regularpay13 + regularpay14
-print(total_regular_pay)
+else:
+    print("Data invalid, no pay calculated")
